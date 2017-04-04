24e20cb3cba371feb269d1b68b5b8245cc24ea28
raphaelclavijo <matthraph@gmail.com>
2017-04-04 14:53:44 +0200

    page Serveur optimisé
    page web optimisée // même css
    
    (répétitions remplacées par création d'input html sous conditions jQuery
    --> un seul traitement PHP)
    ==> page plus compréhensible, plus courte, moins lourde

--- SERVEUR/index.php
+++ SERVEUR/index.php
@@ -12,13 +12,13 @@
 <!-- transition distance / contrôles -->
 
 <!-- action d'écriture après l'action submit jQuery / actualisation des distances-->	<!-- PHP -->
-	<!-- ARRET -->
+	
     <?php
-    if (isset($_POST["STOP"])){ /* isset renvoie le booléen correspondant suivant si STOP est défini (!=NULL) */
-    	$fp = fopen("C:\\wamp64\\www\\RaspberryPI\\moteur\\moteurs.txt","w"); /* Ouverture de moteurs.txt en mode écriture après écrasement du fichier */
-    	$STOP = $_POST["STOP"]; /* variable $STOP correspond au value de l'input de name = "STOP" du form envoyé */
-		$write = fwrite($fp,$STOP); /* écriture dans moteurs.txt de la variable $STOP */
-    	fclose($fp); /* fermeture du fichier moteurs.txt */
+    if (isset($_POST["control"])){ /* isset renvoie le booléen correspondant suivant si control est défini (!=NULL) */
+        $fp = fopen("C:\\wamp64\\www\\RaspberryPI\\moteur\\moteurs.txt","w"); /* Ouverture de moteurs.txt en mode écriture après écrasement du fichier */
+        $control = $_POST["control"]; /* variable $control correspond au value de l'input de name = "control" du form envoyé */
+        fwrite($fp,$control); /* écriture dans moteurs.txt de la variable $control */
+        fclose($fp); /* fermeture du fichier moteurs.txt */
 	/* Lecture des distances */
     	$fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\avant.txt","r");/* Ouverture de avant.txt en mode lecture */
 		$read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
@@ -42,187 +42,52 @@
     	fclose($fp); /* fermeture du fichier gauche.txt */
     }
 	?>
-	<!-- TOUT DROIT -->
-    <?php
-    if (isset($_POST["UP"])){ /* isset renvoie le booléen correspondant suivant si UP est défini (!=NULL) */
-    	$fp = fopen("C:\\wamp64\\www\\RaspberryPI\\moteur\\moteurs.txt","w"); /* Ouverture de moteurs.txt en mode écriture après écrasement du fichier */
-    	$UP = $_POST["UP"]; /* variable $UP correspond au value de l'input de name = "UP" du form envoyé */
-		$write = fwrite($fp,$UP); /* écriture dans moteurs.txt de la variable $UP */
-   	 	fclose($fp); /* fermeture du fichier moteurs.txt */
-	/* Lecture des distances */
-    	$fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\avant.txt","r");/* Ouverture de avant.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='avant'>"; /* block html div permettant une mise en page en css car id ="avant" spécifique à la mesure avant */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier avant.txt */
-        echo "<br/>"; /* retour à la ligne HTML */
-        $fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\droite.txt","r");/* Ouverture de droite.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='droite'>"; /* block html div permettant une mise en page en css car id ="droite" spécifique à la mesure droite */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier droite.txt */
-        echo "<br/>"; /* retour à la ligne HTML */
-        $fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\gauche.txt","r");/* Ouverture de gauche.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='gauche'>"; /* block html div permettant une mise en page en css car id ="gauche" spécifique à la mesure gauche */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier gauche.txt */
-   	 }
-	?>
-	<!-- RECULER -->
-	<?php
-    if (isset($_POST["DOWN"])){ /* isset renvoie le booléen correspondant suivant si DOWN est défini (!=NULL) */
-    	$fp = fopen("C:\\wamp64\\www\\RaspberryPI\\moteur\\moteurs.txt","w"); /* Ouverture de moteurs.txt en mode écriture après écrasement du fichier */
-    	$DOWN = $_POST["DOWN"]; /* variable $DOWN correspond au value de l'input de name = "DOWN" du form envoyé */
-		$write = fwrite($fp,$DOWN); /* écriture dans moteurs.txt de la variable $DOWN */
-   	 	fclose($fp); /* fermeture du fichier moteurs.txt */
-	/* Lecture des distances */
-    	$fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\avant.txt","r");/* Ouverture de avant.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='avant'>"; /* block html div permettant une mise en page en css car id ="avant" spécifique à la mesure avant */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier avant.txt */
-        echo "<br/>"; /* retour à la ligne HTML */
-        $fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\droite.txt","r");/* Ouverture de droite.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='droite'>"; /* block html div permettant une mise en page en css car id ="droite" spécifique à la mesure droite */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier droite.txt */
-        echo "<br/>"; /* retour à la ligne HTML */
-        $fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\gauche.txt","r");/* Ouverture de gauche.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='gauche'>"; /* block html div permettant une mise en page en css car id ="gauche" spécifique à la mesure gauche */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier gauche.txt */
-   	 }
-	?>
-	<!-- TOURNER A DROITE -->
-	<?php
-    if (isset($_POST["RIGHT"])){ /* isset renvoie le booléen correspondant suivant si RIGHT est défini (!=NULL) */
-    	$fp = fopen("C:\\wamp64\\www\\RaspberryPI\\moteur\\moteurs.txt","w"); /* Ouverture de moteurs.txt en mode écriture après écrasement du fichier */
-    	$RIGHT = $_POST["RIGHT"]; /* variable $RIGHT correspond au value de l'input de name = "RIGHT" du form envoyé */
-		$write = fwrite($fp,$RIGHT); /* écriture dans moteurs.txt de la variable $RIGHT */
-   	 	fclose($fp); /* fermeture du fichier moteurs.txt */
-	/* Lecture des distances */
-    	$fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\avant.txt","r");/* Ouverture de avant.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='avant'>"; /* block html div permettant une mise en page en css car id ="avant" spécifique à la mesure avant */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier avant.txt */
-        echo "<br/>"; /* retour à la ligne HTML */
-        $fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\droite.txt","r");/* Ouverture de droite.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='droite'>"; /* block html div permettant une mise en page en css car id ="droite" spécifique à la mesure droite */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier droite.txt */
-        echo "<br/>"; /* retour à la ligne HTML */
-        $fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\gauche.txt","r");/* Ouverture de gauche.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='gauche'>"; /* block html div permettant une mise en page en css car id ="gauche" spécifique à la mesure gauche */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier gauche.txt */
-   	 }
-	?>
-	<!-- TOURNER A GAUCHE -->
-	<?php
-    if (isset($_POST["LEFT"])){ /* isset renvoie le booléen correspondant suivant si LEFT est défini (!=NULL) */
-    	$fp = fopen("C:\\wamp64\\www\\RaspberryPI\\moteur\\moteurs.txt","w"); /* Ouverture de moteurs.txt en mode écriture après écrasement du fichier */
-    	$LEFT = $_POST["LEFT"]; /* variable $LEFT correspond au value de l'input de name = "LEFT" du form envoyé */
-		$write = fwrite($fp,$LEFT); /* écriture dans moteurs.txt de la variable $LEFT */
-   	 	fclose($fp); /* fermeture du fichier moteurs.txt */
-	/* Lecture des distances */
-    	$fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\avant.txt","r");/* Ouverture de avant.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='avant'>"; /* block html div permettant une mise en page en css car id ="avant" spécifique à la mesure avant */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier avant.txt */
-        echo "<br/>"; /* retour à la ligne HTML */
-        $fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\droite.txt","r");/* Ouverture de droite.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='droite'>"; /* block html div permettant une mise en page en css car id ="droite" spécifique à la mesure droite */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier droite.txt */
-        echo "<br/>"; /* retour à la ligne HTML */
-        $fp = fopen("C:\\wamp64\\www\\RaspberryPI\\capteurs\\gauche.txt","r");/* Ouverture de gauche.txt en mode lecture */
-        $read = fgets($fp,10); /* variable $read récupère les caractères situés avant le positionnement 10 du curseur dans le fichier */
-        echo "<div id='gauche'>"; /* block html div permettant une mise en page en css car id ="gauche" spécifique à la mesure gauche */
-        echo $read ; /* affichage de la variable sur la page web */
-        echo "</div>"; /* fermeture block div */
-        fclose($fp); /* fermeture du fichier gauche.txt */
-   	 }
-	?>
-
-<!-- formulaires text moteurs hidden -->
-	<!-- form : lorsqu'il y a inactivité de l'utilisateur -->
-	<form action="" method="post" enctype="multipart/form-data" target="upload_target" id="STOP" > <!-- Toutes les valeurs transmisent sont égales à 0 (value = "0")/(moteurs à l'arrêt) -->
-        <input name="STOP" type="hidden" value="0"/>
-    </form>
-
-	<!-- form : Upload consignes pour moteur -->
-	<form action="" method="post" enctype="multipart/form-data" target="upload_target" id="UP" > <!-- Valeur moteurs UP-->
-        <input name="UP" type="hidden" value="UP"/>
-              
-    </form>
-    <form action="" method="post" enctype="multipart/form-data" target="upload_target" id="DOWN" > <!-- Valeur moteurs DOWN-->
-        <input name="DOWN" type="hidden" value="DOWN"/>
-              
-    </form>
-    <form action="" method="post" enctype="multipart/form-data" target="upload_target" id="RIGHT" > <!-- Valeur moteurs RIGHT-->
-        <input name="RIGHT" type="hidden" value="RIGHT"/>
-              
-    </form>
-    <form action="" method="post" enctype="multipart/form-data" target="upload_target" id="LEFT" > <!-- Valeur moteurs LEFT-->
-        <input name="LEFT" type="hidden" value="LEFT"/>
-              
+	
+<!-- formulaires moteurs -->
+	<form action="" method="post" enctype="multipart/form-data" target="upload_target" id="form" > <!-- upload fait dans la meme page action="" -->
     </form>
 
     <!-- Submit : édite les fichiers de contrôle en envoyant les formulaires -->
 	<script>
-		jQuery(document).ready(function(){ // lorsque jQuery est chargé
+        jQuery(document).ready(function(){ // lorsque jQuery est chargé
 
-			setTimeout(function(){ // delai 1sec moteurs
-				
-				$("#STOP").submit(); // post form "STOP"
-				console.log("STOP") // affiche dans la console STOP --> fonction setTimeout (delay) fonctionne
-  			}, 500);
+            setTimeout(function(){ // delai 0.5 sec moteurs
+                
+                $('#form').html('<input name="control" type="hidden" value="0"/>'); // insertion html dans le formulaire id="form" d'un champ de nom "control" hidden de valeur 0
+                $("#form").submit(); // post form
+                console.log("STOP") // affiche dans la console STOP --> fonction setTimeout (delay) fonctionne
+            }, 500);
 
-			$(document).keypress(function(touche){ // lorsqu'une touche est enfoncé
+            $(document).keypress(function(touche){ // lorsqu'une touche est enfoncé
 
-				var enfonce = touche.which || touche.keyCode; // variable enfonce récupère la valeur de la touche suivant la compréhension du navigateur
-				if(enfonce==38){ // touche flèche directionnelle HAUT test de condition valeur de la touche enfoncé
+                var enfonce = touche.which || touche.keyCode; // variable enfonce récupère la valeur de la touche suivant la compréhension du navigateur
+                if(enfonce==38){ // touche flèche directionnelle HAUT test de condition valeur de la touche enfoncé
 
-				    $("#UP").submit(); // post form "UP"
-				    console.log("UP") // affiche dans la console UP --> fonction keypress fonctionne
-				}
-				if(enfonce==40){ // touche flèche directionnelle DOWN test de condition valeur de la touche enfoncé
+                    $('#form').html('<input name="control" type="hidden" value="UP"/>'); // insertion html dans le formulaire id="form" d'un champ de nom "control" hidden de valeur UP
+                    $("#form").submit(); // post form
+                    console.log("UP") // affiche dans la console UP --> fonction keypress fonctionne
+                }
+                if(enfonce==40){ // touche flèche directionnelle DOWN test de condition valeur de la touche enfoncé
 
-				    $("#DOWN").submit(); // post form "DOWN"
-				    console.log("DOWN") // affiche dans la console DOWN --> fonction keypress fonctionne
-				}
-				if(enfonce==39){ // touche flèche directionnelle RIGHT test de condition valeur de la touche enfoncé
+                    $('#form').html('<input name="control" type="hidden" value="DOWN"/>'); // insertion html dans le formulaire id="form" d'un champ de nom "control" hidden de valeur DOWN
+                    $("#form").submit(); // post form
+                    console.log("DOWN") // affiche dans la console DOWN --> fonction keypress fonctionne
+                }
+                if(enfonce==39){ // touche flèche directionnelle RIGHT test de condition valeur de la touche enfoncé
 
-				    $("#RIGHT").submit(); // post form "RIGHT"
-				    console.log("RIGHT") // affiche dans la console RIGHT --> fonction keypress fonctionne
-				}
-				if(enfonce==37){ // touche flèche directionnelle LEFT test de condition valeur de la touche enfoncé
+                    $('#form').html('<input name="control" type="hidden" value="RIGHT"/>'); // insertion html dans le formulaire id="form" d'un champ de nom "control" hidden de valeur RIGHT
+                    $("#form").submit(); // post form
+                    console.log("RIGHT") // affiche dans la console RIGHT --> fonction keypress fonctionne
+                }
+                if(enfonce==37){ // touche flèche directionnelle LEFT test de condition valeur de la touche enfoncé
 
-				    $("#LEFT").submit(); // post form "LEFT"
-				    console.log("LEFT") // affiche dans la console LEFT --> fonction keypress fonctionne
-				}
-			});
-		});
-	</script>
+                    $('#form').html('<input name="control" type="hidden" value="LEFT"/>'); // insertion html dans le formulaire id="form" d'un champ de nom "control" hidden de valeur LEFT
+                    $("#form").submit(); // post form
+                    console.log("LEFT") // affiche dans la console LEFT --> fonction keypress fonctionne
+                }
+            });
+        });
+    </script>
 
 </body>
 </html>
